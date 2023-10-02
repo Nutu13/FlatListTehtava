1aa2cba2b82b78642dd48d77811afbeb08da6a2e
IAmSnode <nuutti28@gmail.com>
2023-10-02 15:23:24 +0300

    Lista toimii

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { FlatList, SafeAreaView, StyleSheet } from "react-native";
+import { DATA } from "./Data";
+import Row from "./components/Row";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        style={{ width: "100%", padding: 10, marginTop: 50 }}
+        data={DATA}
+        renderItem={({ item }) => <Row person={item} />}
+      ></FlatList>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ Data.js
@@ -0,0 +1,57 @@
+export const DATA = [
+  {
+    id: "1",
+    lastname: "Doe",
+    firstname: "John",
+  },
+  {
+    id: "2",
+    lastname: "Doe",
+    firstname: "Jane",
+  },
+  {
+    id: "3",
+    lastname: "Polo",
+    firstname: "Marco",
+  },
+  {
+    id: "4",
+    lastname: "Huikkanen",
+    firstname: "Jussi",
+  },
+  {
+    id: "5",
+    lastname: "Makkonen",
+    firstname: "Matti",
+  },
+  {
+    id: "6",
+    lastname: "Korhonen",
+    firstname: "Erkki",
+  },
+  {
+    id: "7",
+    lastname: "Korhonen",
+    firstname: "Seija",
+  },
+  {
+    id: "8",
+    lastname: "Korhonen",
+    firstname: "Kalle",
+  },
+  {
+    id: "9",
+    lastname: "Korhonen",
+    firstname: "Maikki",
+  },
+  {
+    id: "10",
+    lastname: "Korhonen",
+    firstname: "Mikko",
+  },
+  {
+    id: "11",
+    lastname: "Korhonen",
+    firstname: "Maija",
+  },
+];
